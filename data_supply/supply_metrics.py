aa7c92057936e5ef8ee92f073a6a81873685b903
Ian List <ian@fjodor.me>
2021-04-06 19:08:42 +0200

    Added possible command-line arguments to data_supply script

--- data_supply/supply_metrics.py
+++ data_supply/supply_metrics.py
@@ -2,12 +2,17 @@ import psutil
 import json
 import ssl
 import websocket
+import getopt
 
 from time import sleep
+from sys import argv
 
 ws = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
 
-def send_data(uri="ws://localhost:8000/produce_metric", hostname="Gort"):
+short_options = "hu:n:"
+long_options = ["help", "uri=", "hostname="]
+
+def send_data(uri="ws://localhost:8000/produce_metric", hostname="yourhost"):
     ws.connect(uri)
     unique_id = ws.recv()
     unique_id = json.loads(unique_id).get("id")
@@ -18,7 +23,7 @@ def send_data(uri="ws://localhost:8000/produce_metric", hostname="Gort"):
         payload = {
                     "id": unique_id,
                     "hostname": hostname,
-                    "cpu_average": cpu_average,
+                    "cpu_average": float(cpu_average),
                     "cpus": list((enumerate(cpu_stats))),
                     "ram_used": virtual_memory.total - virtual_memory.available,
                     "ram_total": virtual_memory.total
@@ -27,4 +32,16 @@ def send_data(uri="ws://localhost:8000/produce_metric", hostname="Gort"):
         sleep(1)
 
 if __name__ == "__main__":
-    send_data()
+    data_args = {}
+    try:
+        arguments, values = getopt.getopt(argv[1:], short_options, long_options)
+    except getopt.error as err:
+        print(str(err))
+    for argument, value in arguments:
+        if argument == "-h" or argument == "--help":
+            print("TODO: display help")
+        if argument == "-u" or argument == "--uri":
+            data_args["uri"] = value
+        if argument == "-n" or argument == "--hostname":
+            data_args["hostname"] = value
+    send_data(**data_args)
